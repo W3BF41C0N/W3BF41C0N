333a4d2c173eccc214111f332bb11b59859b5e7c
W3BF41C0N <92768861+W3BF41C0N@users.noreply.github.com>
2021-10-19 07:57:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from speedtest import Speedtest
+
+st = Speedtest()
+
+print("Your Connection's Download Speed is:" , st.download())
+print("Your Connection's Upload Speed is :" , st.upload())
+print ("THANK YOU USE THIS TOOL ")
+
